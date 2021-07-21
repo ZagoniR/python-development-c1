e34e7cd2f242bd585576b8970e7eb1f334405e96
Zagoni Robert <zagoni_robert@yahoo.com>
2021-07-21 08:02:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a comment line
+
+"""
+This is a comment on multiple lines
+"""
+
+my_sum = 1 + 2  # This is an inline comment
+
+# ctrl + d, duplicates the selected line(s)
+print("Hello, World!")
+print("Hello, World!")
+
+# r in front of a string cancels the special characters (e.g. escape characters)
+print("Hello, \n World!")
+print(r"Hello, \n World!")
